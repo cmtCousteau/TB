4aa288d2404c6ad9ae2227866f91fa958df166df
CmtCousteau <marco.mon@windowslive.com>
2017-05-22 20:18:47 +0200

    te

--- MyApplication/app/src/main/java/com/example/marco/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/marco/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothGattDescriptor;
 
 
 import android.content.Intent;
@@ -30,7 +31,7 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-//caca
+
     private BluetoothAdapter mBluetoothAdapter  = null;
     private BluetoothLeScanner mBluetoothLeScanner = null;
 
@@ -41,11 +42,17 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultTextView = null;
     private Button btnStartScan = null;
     private Button btnStopScan = null;
+    private Button btnRead = null;
+    private Button btnDisconnect = null;
+
+    private int mStatus;
 
     private Handler mHandler;
 
     public static final int REQUEST_BT_PERMISSIONS = 0;
     public static final int REQUEST_BT_ENABLE = 1;
+
+    public ArrayList<BluetoothGattCharacteristic> charas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +60,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mHandler = new Handler();
-
+        // Textview contenant les résultats
         resultTextView = (TextView)findViewById(R.id.txvResult);
 
+        // Bouton de scan
         btnStartScan = (Button)findViewById(R.id.btnStartScan);
         btnStartScan.setOnClickListener(startScanListener);
-
+        // Bouton pour arrêter le scan
         btnStopScan = (Button)findViewById(R.id.btnStopScan);
         btnStopScan.setOnClickListener(stopScanListener);
+        // Bouton pour afficher les résultats
+        btnRead = (Button)findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(btnReadListener);
+        // Bouton de déconnection
+        btnDisconnect = (Button)findViewById(R.id.btnDisconnect);
+        btnDisconnect.setOnClickListener(btnDisconnectListener);
+
+
 
         this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -68,14 +84,22 @@ public class MainActivity extends AppCompatActivity {
 
         mBluetoothAdapter.getBluetoothLeScanner();
         this.mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+    }
 
-        checkBtPermissions();
-        enableBt();
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        init();
     }
 
     public Context getContext() {
         return (Context)this;
+    }
+
+
+    private void init(){
+        checkBtPermissions();
+        enableBt();
     }
 
     public void enableBt(){
@@ -88,12 +112,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // ATTENTIUON VERSION 23 SEULEMENT
     public void checkBtPermissions() {
-        this.requestPermissions(
-                new String[]{
-                        Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_COARSE_LOCATION
-                },
-                REQUEST_BT_PERMISSIONS);
+       // this.requestPermissions(
+         //       new String[]{
+           //             Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_COARSE_LOCATION
+         //       },
+          //      REQUEST_BT_PERMISSIONS);
     }
 
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
@@ -101,14 +126,14 @@ public class MainActivity extends AppCompatActivity {
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
 
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                //resultTextView.setText("Connected to GATT server.");
                 Log.i(TAG, "Connected to GATT server.");
-
+                mStatus = newState;
                 mBluetoothGatt.discoverServices();
+                //gatt.discoverServices();
 
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 Log.i(TAG, "Disconnected from GATT server.");
-                //resultTextView.setText("Disconnected from GATT server.");
+                mStatus = newState;
             }
         }
 
@@ -119,24 +144,26 @@ public class MainActivity extends AppCompatActivity {
 
                 List<BluetoothGattService> services = gatt.getServices();
                 List<BluetoothGattCharacteristic> gattCharacteristics;
-                ArrayList<BluetoothGattCharacteristic> charas;
-
+                //ArrayList<BluetoothGattCharacteristic> charas;
+                charas = new ArrayList<>();
                 for (BluetoothGattService gattService : services) {
-                    Log.i(TAG, "---------------------------------------------------------- : ");
-                    Log.i(TAG, "Service : " + gattService);
+                   // Log.i(TAG, "---------------------------------------------------------- : ");
+                   // Log.i(TAG, "Service : " + gattService);
                     gattCharacteristics = gattService.getCharacteristics();
-                    charas = new ArrayList<>();
                     for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
-
+                       // gatt.readCharacteristic(gattCharacteristic);
                         charas.add(gattCharacteristic);
-                        Log.i(TAG, "Chara uuid    : " + gattCharacteristic.getUuid());
-                        Log.i(TAG, "read attempt  : " + gatt.readCharacteristic(gattCharacteristic));
-                   //     Log.i(TAG, "Write attempt : " + gatt.writeCharacteristic(gattCharacteristic));
-                        Log.i(TAG, "permission    : " + gattCharacteristic.getPermissions());
+                      //  Log.i(TAG, "Chara uuid    : " + gattCharacteristic.getUuid());
+                       // Log.i(TAG, "read attempt  : " + gatt.readCharacteristic(gattCharacteristic));
+                   //   Log.i(TAG, "Write attempt : " + gatt.writeCharacteristic(gattCharacteristic));
+                      //  Log.i(TAG, "permission    : " + gattCharacteristic.getPermissions());
 
                         // recpère le nom
-                        /*if(gattCharacteristic.getUuid().toString().equalsIgnoreCase("00002a00-0000-1000-8000-00805f9b34fb")) {
+                       /* if(gattCharacteristic.getUuid().toString().equalsIgnoreCase("3347ab01-fb94-11e2-a8e4-f23c91aec05e")) {
+
+                            gatt.get
                             Log.i(TAG, "lecture : " + gatt.readCharacteristic(gattCharacteristic));
+
                         }
                         if(gattCharacteristic.getUuid().toString().equalsIgnoreCase("3347aad2-fb94-11e2-a8e4-f23c91aec05e")) {
                             Log.i(TAG, "test");
@@ -150,25 +177,26 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        @Override
+       /* @Override
         public void onCharacteristicRead(BluetoothGatt gatt,BluetoothGattCharacteristic characteristic, int status) {
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                Log.i(TAG, "Read chara : ----------------------------");
+               // Log.i(TAG, "Read chara : ----------------------------");
                 Log.i(TAG, characteristic.getStringValue(0));
+                //Log.i(TAG, "Read chara : ----------------------------");
             }
-        }
+        }*/
 
-        @Override
+       /* @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-        }
+        }*/
 
-        @Override
+      /*  @Override
         public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             Log.i(TAG, "cacacacacaca : ");
             super.onCharacteristicWrite(gatt, characteristic, status);
             boolean stat = characteristic.setValue("0");
             Log.i(TAG, "Write result : " + stat);
-        }
+        }*/
     };
 
     private ScanCallback mLeScanCallback =
@@ -183,14 +211,12 @@ public class MainActivity extends AppCompatActivity {
                     ViewGroup linearLayout = (ViewGroup) findViewById(R.id.linearLayoutID);
                     linearLayout.addView(newButton);*/
 
-
+                    // Si le device trouvé est le sable-x
                     if(result.getDevice().getName() != null && result.getDevice().getName().contains("Sa")) {
                         resultTextView.setText("Name\t\t\t: " + result.getDevice().getName() + "\n MAC adress\t: " + result.getDevice().getAddress());
-
-                       // result.getDevice().createBond();
                         BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(result.getDevice().getAddress());
-                        mBluetoothGatt=device.connectGatt(MainActivity.this, false, mGattCallback);
-                        //mBluetoothGatt = result.getDevice().connectGatt(MainActivity.this, false, mGattCallback);
+                        // Connection au gatt
+                        mBluetoothGatt = device.connectGatt(MainActivity.this, false, mGattCallback);
                         mBluetoothLeScanner.stopScan(mLeScanCallback);
                     }
                 }
@@ -200,6 +226,34 @@ public class MainActivity extends AppCompatActivity {
                     super.onScanFailed(errorCode);
                 }
             };
+
+    private void readTest(){
+        resultTextView.setText("read en cours");
+
+        BluetoothGattCharacteristic ch;
+        for (BluetoothGattCharacteristic gattCharacteristic : charas) {
+            if( gattCharacteristic.getUuid().toString().equals("3347ab01-fb94-11e2-a8e4-f23c91aec05e")) {
+                ch =  gattCharacteristic;
+                mBluetoothGatt.readCharacteristic(gattCharacteristic);
+                Log.i(TAG, "Chara uuid    : " + ch.getUuid());
+
+                if(ch.getValue() != null) {
+                    Log.i(TAG, "Chara value   : " + new String(ch.getValue()));
+                }
+
+                byte uncompressed[] = ("sdadsa").getBytes();
+                ch.setValue(uncompressed);
+                mBluetoothGatt.writeCharacteristic(ch);
+
+               /* for (BluetoothGattDescriptor descriptor:gattCharacteristic.getDescriptors()){
+                    //mBluetoothGatt.readDescriptor(descriptor);
+                    Log.e(TAG, "BluetoothGattDescriptor : " + descriptor.getUuid().toString() + " Value : " + descriptor.getValue());
+                }*/
+
+                //
+            }
+        }
+    }
 
     private OnClickListener startScanListener = new OnClickListener() {
 
@@ -215,6 +269,24 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             resultTextView.setText("stop");
             mBluetoothLeScanner.stopScan(mLeScanCallback);
+        }
+    };
+
+    private OnClickListener btnReadListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            resultTextView.setText("read");
+            readTest();
+        }
+    };
+
+    private OnClickListener btnDisconnectListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            resultTextView.setText("disconnected");
+            Log.i(TAG, "BLEd device disconnected.");
+            //mBluetoothGatt.close();
+            mBluetoothGatt.disconnect();
         }
     };
 }
